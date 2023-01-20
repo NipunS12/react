0f6530f91b7a0845574d9b3403f5839245828a7f
NipunS12 <nipunsandeepa308@gmail.com>
2023-01-20 10:51:28 +0530

    clean

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -3,31 +3,31 @@ import React from 'react'
 const Project = () => {
   return (
     <div className='projects'>
-        <div className="projectContent">
+      <div className="projectContent">
         <h1>Projects</h1>
         <hr />
-        </div> 
+      </div>
 
-        <div className="form">
+      <div className="form">
         <div className="inputs">
-            <label htmlFor="">Name</label>
-            <input type="text" />
-        </div> 
-
-         <div className="inputs">
-            <label htmlFor="">Email</label>
-            <input type="text" />
+          <label htmlFor="">Name</label>
+          <input type="text" />
         </div>
 
-         <div className="inputs">
-            <label htmlFor="">Message</label>
-            <input className='Message' type="text" />
+        <div className="inputs">
+          <label htmlFor="">Email</label>
+          <input type="text" />
         </div>
-        <button className='sendButton'>Send</button> 
+
+        <div className="inputs">
+          <label htmlFor="">Message</label>
+          <input className='Message' type="text" />
         </div>
+        <button className='sendButton'>Send</button>
+      </div>
     </div>
 
-    
+
   )
 }
 
